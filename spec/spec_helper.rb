f5fbef580a1543998d67a0b3ddcbdd65d3819816
jaig-sticky <jerome.aiguillon@stickyads.tv>
2021-12-12 15:59:23 +0100

    Fix carriage return

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
 # spec_helper.rb
 require 'capybara/rspec'
 require 'capybara/dsl'
-require_relative './helper/BS_Formatter.rb
-'
+require_relative './helper/BS_Formatter'
+
 # set up driver
 Capybara.register_driver :headless_chrome do |app|
   Capybara::Selenium::Driver.new(
