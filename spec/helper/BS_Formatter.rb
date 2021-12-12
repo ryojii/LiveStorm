a14e0f720aeeebef75f5ffb94312387cc5db979f
jaig-sticky <jerome.aiguillon@stickyads.tv>
2021-12-12 16:52:51 +0100

    Fix code (put/puts)

--- spec/helper/BS_Formatter.rb
+++ spec/helper/BS_Formatter.rb
@@ -14,13 +14,13 @@ class BS_Formatter
   end
 
   def example_passed notification # ExampleNotification
-    put (' {"status":"passed", "reason": "' + notification.full_description + '"}}')
+    puts (' {"status":"passed", "reason": "' + notification.description + '"}}')
     #@driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"passed", "reason": "' + notification.full_description + '"}}')
     #@driver.quit
   end
 
   def example_failed notification # FailedExampleNotification
-    put (' {"status":failed", "reason": "' + notification.full_description + '"}}')
+    puts (' {"status":failed", "reason": "' + notification.description + '"}}')
     #@driver.execute_script('browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed", "reason": "' + notification.full_description + '"}}')
     #@driver.quit
   end
