c584d3c766f71d70be5c7fa87f924feda3ebe6ba
JAiguillon <aiguillon.jerome@gmail.com>
2021-12-14 11:49:16 +0100

    Update spec_helper.rb
    remove comment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,11 +6,6 @@ require 'selenium-webdriver'
 
 require_relative './helper/BS_Formatter'
 
-## set up driver as a global var, I suppose there is a better way to do this.
-#if $driver == nil 
-#  $driver = Selenium::WebDriver.for(:remote, :url => "https://jeromeaiguillon_WT6VE1:Xq2JZQvpx4U23zoKTUCw@hub-cloud.browserstack.com/wd/hub")
-#end
-
 # configure
 Capybara.configure do |config|
   config.ignore_hidden_elements = true
