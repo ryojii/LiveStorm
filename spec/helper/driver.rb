f562d07612f5b0a118b5491f4b10aecb06641080
JAiguillon <aiguillon.jerome@gmail.com>
2021-12-24 15:39:59 +0100

    Update driver.rb

--- spec/helper/driver.rb
+++ spec/helper/driver.rb
@@ -6,10 +6,10 @@ class Driver
 
   def initialize()
     if $driver == nil 
-      $driver = Selenium::WebDriver.for(:remote, :url => "https://jeromeaiguillon_WT6VE1:Xq2JZQvpx4U23zoKTUCw@hub-cloud.browserstack.com/wd/hub")
+      $driver = Selenium::WebDriver.for(:remote, :url => "https://user:tokenw@hub-cloud.browserstack.com/wd/hub")
     else
       $driver = nil
-      $driver = Selenium::WebDriver.for(:remote, :url => "https://jeromeaiguillon_WT6VE1:Xq2JZQvpx4U23zoKTUCw@hub-cloud.browserstack.com/wd/hub")
+      $driver = Selenium::WebDriver.for(:remote, :url => "https://user:token@hub-cloud.browserstack.com/wd/hub")
     end
   end
 end
